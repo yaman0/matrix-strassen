d3814c43e263ce30605db083a1ed217d35d37279
yamano <yaman0@protonmail.com>
2019-01-20 23:36:02 +0100

    feat(main): create interactive form to multiply 2 matrix in input

--- src/main.py
+++ src/main.py
@@ -1,21 +1,30 @@
 from Matrix import Matrix
-#
-# d = [[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4]]
-d = [[2,3,4,5],[2,2,2,2],[2,2,2,2],[2,2,2,2]]
-d2 = [[4,5,2,2],[2,3,3,4],[2,3,3,4],[2,3,3,4]]
-# # d2 = [[4,5,1],[2,3,1]]
-a = Matrix(d)
-b = Matrix(d2)
-print(a.strassen(b))
-# a = Matrix([[1,1], [1,1]])
-# a=a.horizontaljoin(a)
-# print a
-# print "rrr"
-# a=a.verticaljoin(a)
-# print a
-# print "rrr"
-#
-# a=a.horizontaljoin(a)
-# print a
-# print 'rrrr'
-# print(a.verticaljoin(a))
+import ast
+
+
+def inputMatrix():
+    print "Get matrix (ex: [[2,3,1],[1,5,6],[4,5,7]]:"
+    string = raw_input()
+    tab = ast.literal_eval(string)
+    return Matrix(tab)
+
+
+def getTypeMulti():
+    print "Strassen(s)/Classic(c):"
+    string = raw_input()
+    if not len(string) == 1:
+        raise ValueError('Need to write 1 char')
+    if string == 'c':
+        return 1
+    if string == 's':
+        return 2
+    raise ValueError('Need to write c or s')
+
+
+a = inputMatrix()
+b = inputMatrix()
+operation = getTypeMulti()
+if operation == 1:
+    print a.strassen(b)
+if operation == 2:
+    print a * b

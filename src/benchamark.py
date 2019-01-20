daf3216d5a541cbae0e91318e49e95467899b8b5
yamano <yaman0@protonmail.com>
2019-01-20 16:47:46 +0100

    Feat:Remove matplot to benchrmark and just create an out csv file

--- src/benchamark.py
+++ src/benchamark.py
@@ -1,36 +1,43 @@
-import matplotlib.pyplot as plt
+import calendar
+import datetime
+import os
 import sys
 import time
 from Matrix import Matrix
 
+# create subdirectory out
+if not os.path.exists('../out'):
+    os.makedirs('../out')
+file = open("../out/out" + str(calendar.timegm(time.gmtime())) + ".csv", "w")
+
 sys.setrecursionlimit(10000)
-SIZE = 2100
+SIZE = 1000
 j = 2
 a = Matrix([[1, 1], [1, 1]])
 
-indexTab = []
-simpleTab = []
-strassenTab = []
+file.write("n;classic;strassen\n")
 
 while j <= SIZE:
-    time_before = time.time() * 1000.0
-    a = a * a
-    time_after = time.time() * 1000.0
-    simpleTab.append(time_after - time_before)
+    string = ''
 
+    string += str(j) + ";"
+    # classic
+    time_before = time.time() * 1000.0
+    classic = a * a
+    time_after = time.time() * 1000.0
+    string += str(time_after - time_before) + ';'
+
+    # strassen
     time_before = time.time() * 1000.0
     strassen = a.strassen(a)
     time_after = time.time() * 1000.0
-    strassenTab.append(time_after - time_before)
+    string += str(time_after - time_before) + '\n'
 
-    indexTab.append(j)
+    file.write(string)
+    print(str(j))
     j *= 2
 
+    # prepare matrix
     a = a.horizontaljoin(a)
     a = a.verticaljoin(a)
-
-    plt.plot(indexTab, simpleTab, label='Simple')
-    plt.plot(indexTab, strassenTab, label='Strassen')
-    plt.legend()
-    plt.show()
-    plt.savefig("out/" + str(j) + ".png")
+file.close()
